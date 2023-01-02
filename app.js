526bf0cf57aeba62dc64f51f726e41a3b271eb61
Avinash Karmjit <rk442537@gmail.com>
2023-01-03 01:09:29 +0530

    confugured for cyclic.sh hosting

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const PORT = process.env.PORT || 3000
 // for cyclic.sh hosting
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+        const conn = await mongoose.connect(process.env.MONGO_URL);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.log(error);
